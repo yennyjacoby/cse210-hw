e43d1a7f6b06250d20d92557915f4d7468b9132a
yennyjacoby <yekaromeroro@gmail.com>
2026-09-14 14:58:37 -0700

    W03 Learning Activity, Encapsulation

---
+++ week03/Fractions/fraction.cs
@@ -0,0 +1,44 @@
+//hold fraction
+//It should have two privates attributes for the top and bottom numbers
+
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+public class Fraction
+
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top =1;
+        _bottom =1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom =1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top= top;
+        _bottom = bottom;
+    }
+
+    // Methods
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
+
